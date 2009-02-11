97ac2d98b4b10d49d41508187804ecb9a9245e16
jolts <johan@sharpcode.se>
2009-02-12 00:06:42 +0100

    fixed indent

--- lib/parsedirectory.rb
+++ lib/parsedirectory.rb
@@ -3,24 +3,21 @@ require 'fileutils'
 
 module TildeSorter
 
-class DirectoryParser
-  attr_accessor :pictures, :torrents
+  class DirectoryParser
+    attr_accessor :pictures, :torrents
 
-  def initialize(directory)
-    @directory = directory
-  end
+    def initialize(directory)
+      @directory = directory
+    end
 
-  def directories
-    dirs = Array.new
-    IO.popen("ls -l #{@directory} | grep '^d'").each do |dir|
-      dirs << dir
-          end
-    dirs
-end
-end
-
-
-
-dir = DirectoryParser.new("/home/johan")
-puts dir.directories
+    def directories
+      dirs = Array.new
+      IO.popen("ls -l #{@directory} | grep '^d'").each do |dir|
+        dirs << dir
+      end
+      dirs
+    end
+  
+  dir = DirectoryParser.new("/home/johan")
+  puts dir.directories
 end
