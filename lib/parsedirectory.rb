514fb304812a24d325f006722b65259cfd66e05c
Gigamo <gigamo@gmail.com>
2009-02-13 15:41:35 +0100

    fixed directory parser

--- lib/parsedirectory.rb
+++ lib/parsedirectory.rb
@@ -1,23 +1,13 @@
 #!/usr/bin/env ruby
-require 'fileutils'
 
 module TildeSorter
-
   class DirectoryParser
-    attr_accessor :pictures, :torrents
-
-    def initialize(directory)
+    def self.directories(directory)
       @directory = directory
-    end
 
-    def directories
-      dirs = Array.new
-      IO.popen("ls -l #{@directory} | grep '^d'").each do |dir|
-        dirs << dir
-      end
-      dirs
+      puts Dir[@directory+"/**"]
     end
-  
-  dir = DirectoryParser.new("/home/johan")
-  puts dir.directories
+  end
 end
+
+puts TildeSorter::DirectoryParser.directories("/home/gig")
