857615e182ad6fb1bd2dbc6a41664d54ec3e586d
Gigamo <gigamo@gmail.com>
2009-02-13 22:17:21 +0100

    Use optionparser

--- tildesorter.rb
+++ tildesorter.rb
@@ -1,50 +1,83 @@
 #!/usr/bin/env ruby
 require 'fileutils'
+require 'optparse'
+require 'optparse/time'
 
-module Parser
-  class Parse
-  end
-
-  class DirectoryParser < Parse
-    def initialize(directory=ENV['HOME'])
-      @@directory = directory
+module TildeSorter
+  module Parser
+    class Parse
     end
 
-    def directories
-      puts Dir.glob("#{@@directory}/**")
-    end
-  end
-
-  class FileParser < Parse
-    def initialize(*exts)
-      @@exts = exts
+    class DirectoryParser < Parse
+      def self.directories
+        puts Dir.glob("#{$parse_dir}/**")
+      end
     end
 
-    def files
-      FileUtils.cd(ENV['HOME'])
-      @@exts.each do |ext|
-        puts Dir.glob("**/*.#{ext}")
+    class FileParser < Parse
+      def self.files
+        old_dir = Dir.getwd
+        FileUtils.cd(ENV['HOME'])
+        $exts.each do |ext|
+          puts Dir.glob("**/*.#{ext}")
+        end
+        FileUtils.cd(old_dir)
       end
     end
   end
-end
 
-module Sorter
-  class Sort
+
+  module Sorter
+    class Sort
+    end
+
+    class PDFSort < Sort
+    end
   end
 
-  class PDFSort < Sort
+  
+  class Opts
+    def self.parse(args)
+      opts = OptionParser.new do |opts|
+        opts.banner = "Usage: #{$0} [options]"
+        
+        opts.separator ""
+        opts.separator "Specific options:"
+
+        opts.on("--parsedir PARSEDIR", "The directory to parse") do |p|
+          $parse_dir = p
+        end
+
+        opts.on("--exts pdf,avi,mp3", Array, "Extensions to search for") do |e|
+          $exts = e
+        end
+
+        opts.separator ""
+        opts.separator "Common options:"
+
+        opts.on_tail("-h", "--help", "Show this message") do
+          puts opts
+          exit
+        end
+      end
+      opts.parse!(args)
+    end
+  end
+
+
+  def self.run
+    # Defaults
+    $parse_dir = ENV['HOME']
+    $exts = "pdf"
+    
+    # Parse user options
+    Opts.parse(ARGV)
+    
+    # Run program
+    Parser::DirectoryParser.directories
+    Parser::FileParser.files
   end
 end
 
 
-#
-puts "Directories:\n\n"
-dp = Parser::DirectoryParser.new
-dp.directories
-
-puts "\n---\n\n"
-
-puts "Files:\n\n"
-fp = Parser::FileParser.new("pdf", "avi")
-fp.files
+TildeSorter::run if $0 == __FILE__ and not ARGV.empty?
