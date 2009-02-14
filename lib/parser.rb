75281ef17befd983ba2d8dc7303ae2ae43681a6b
Gigamo <gigamo@gmail.com>
2009-02-14 12:20:40 +0100

    Removed unnecessary cd

--- lib/parser.rb
+++ lib/parser.rb
@@ -13,13 +13,11 @@ module TildeSorter
 
     class FileParser < Parse
       def self.files
-        old_dir = Dir.getwd
         FileUtils.cd(ENV['HOME'])
         Config.exts.each do |ext|
           puts Dir.glob("**/*.#{ext}")
         end
-        FileUtils.cd(old_dir)
-        ## FileUtils.cd(File.path(__FILE__))
+        ## FileUtils.cd(File.dirname(__FILE__))
       end
     end
   end
